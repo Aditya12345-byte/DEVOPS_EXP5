ccf344378fae523ef353e83a1bb9d274756f0355
JADHAV ADITYA SUJIT <5023119@it.fcrit.ac.in>
2026-03-02 15:22:12 +0530

    Updated AdditionTest to work on Jenkins workspace dynamically

--- src/Test/java/com/example/selenium/AdditionTest.java
+++ src/Test/java/com/example/selenium/AdditionTest.java
@@ -2,6 +2,8 @@ package com.example;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,23 +17,39 @@ public class AdditionTest {
     @Test
     public void testAddition() throws InterruptedException {
 
+        // Setup ChromeDriver automatically
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
-        driver.get("file:///S:/webapp/src/main/webapp/index.html");
+        try {
+            // Get the Jenkins workspace path dynamically
+            String workspace = System.getProperty("user.dir"); 
+            // Build the file path to index.html
+            File htmlFile = new File(workspace + "/src/main/webapp/index.html");
 
-        driver.findElement(By.id("num1")).sendKeys("5");
-        driver.findElement(By.id("num2")).sendKeys("7");
+            // Open the HTML file
+            driver.get("file:///" + htmlFile.getAbsolutePath().replace("\\", "/"));
 
-        driver.findElement(By.tagName("button")).click();
+            // Fill in the numbers
+            driver.findElement(By.id("num1")).sendKeys("5");
+            driver.findElement(By.id("num2")).sendKeys("7");
 
-        Thread.sleep(2000);
+            // Click the Add button
+            driver.findElement(By.tagName("button")).click();
 
-        WebElement result = driver.findElement(By.id("result"));
-        String text = result.getText();
+            // Wait a moment for the result
+            Thread.sleep(1000);
 
-        assertEquals("Result: 12", text);
+            // Get the result text
+            WebElement result = driver.findElement(By.id("result"));
+            String text = result.getText();
 
-        driver.quit();
+            // Check the result
+            assertEquals("Result: 12", text);
+
+        } finally {
+            // Quit driver in all cases
+            driver.quit();
+        }
     }
 }
